4a2648f5ba0b95a0ca9a47655092b1848550634a
yohan <beginnerdevyohan@gmail.com>
2026-02-22 16:59:12 -0500

    Initial Commit

---
+++ DLOG/DLOGApp.swift
@@ -0,0 +1,17 @@
+//
+//  DLOGApp.swift
+//  DLOG
+//
+//  Created by Yohan Yoon on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct DLOGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
