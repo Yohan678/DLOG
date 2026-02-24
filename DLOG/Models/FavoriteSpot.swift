e5b818adf25eaa9c74ebb1343fa4d30286785330
yohan <beginnerdevyohan@gmail.com>
2026-02-24 00:48:10 -0500

    HomeView Edit

---
+++ DLOG/Models/FavoriteSpot.swift
@@ -0,0 +1,8 @@
+//
+//  FavoriteSpot.swift
+//  DLOG
+//
+//  Created by Yohan Yoon on 2/24/26.
+//
+
+import Foundation
---
+++ DLOG/Views/HomeCategoryBoxView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeCategoryBoxView.swift
+//  DLOG
+//
+//  Created by Yohan Yoon on 2/23/26.
+//
+
+import SwiftUI
+
+struct HomeCategoryBoxView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeCategoryBoxView()
+}
